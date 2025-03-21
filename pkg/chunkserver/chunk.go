0c466e3c2cfa62a564d452fb14b6bf8139b5c166
Saurav Fouzdar <sauravfouzdar@Sauravs-MacBook-Pro.local>
2025-03-21 13:36:07 +0530

    Add NewChunk()

---
+++ pkg/chunkserver/chunk.go
@@ -0,0 +1,27 @@
+package chunkserver
+
+import (
+	"sync"
+
+	"github.com/sauravfouzdar/pkg/common"
+)
+
+
+type Chunk struct {
+	Username		common.ChunkUsername
+	Version			common.ChunkVersion
+	Size			uint64
+	mutex			sync.RWMutex
+}
+
+// NewChunk creates a new chunk
+func NewChunk(username common.ChunkUsername) *Chunk {
+	return &Chunk{
+		Username: username,
+		Version: 0,
+		Size: 0,
+	}
+}
+
+
+
